33e61d9f171093070a3942e5bdb0085e8da12f4f
ROSERANI484 <roserani484@gmail.com>
2024-10-07 16:13:58 +0530

    img

--- about.php
+++ about.php
@@ -15,7 +15,7 @@
       <div class="row">
         <div class="col-lg-12">
           <h6>Get all details</h6>
-          <h2>Online Teaching and Learning Tools</h2>
+          <h2>about the university</h2>
         </div>
       </div>
     </div>

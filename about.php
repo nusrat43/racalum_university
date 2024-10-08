616efcaa651775843dce214a58c4630fa3cbe5de
ROSERANI484 <roserani484@gmail.com>
2024-10-08 18:30:39 +0530

    img

--- about.php
+++ about.php
@@ -21,83 +21,77 @@
     </div>
   </section>
 
-  <section class="meetings-page" id="meetings">
+  <section class="meetings-page" id="meetings "style="background-image: url('assets/images/crop_img3.jpg');; background-size: cover; background-position: center;">
     <div class="container">
-      <div class="row">
-        <div class="col-lg-12">
-          <div class="row">
+        <div class="row">
             <div class="col-lg-12">
-              <div class="meeting-single-item">
-                <div class="thumb">
-                  <div class="price">
-                    <span>$14.00</span>
-                  </div>
-                  <div class="date">
-                    <h6>Nov <span>12</span></h6>
-                  </div>
-                  <a href="meeting-details.html"><img src="assets/images/single-meeting.jpg" alt=""></a>
-                </div>
-                <div class="down-content">
-                  <a href="meeting-details.html"><h4>Online Teaching and Learning Tools</h4></a>
-                  <p>Recreio dos Bandeirantes, Rio de Janeiro - RJ, 22795-008, Brazil</p>
-                  <p class="description">
-                    This is an edu meeting HTML CSS template provided by <a href="https://templatemo.com/" target="_blank" rel="nofollow">TemplateMo website</a>. This is a Bootstrap v5.1.3 layout. If you need more free website templates like this one, please visit our website TemplateMo. Please tell your friends about our website. Thank you. If you want to get the latest collection of HTML CSS templates for your websites, you may visit <a rel="nofollow" href="https://www.toocss.com/" target="_blank">Too CSS website</a>. If you need a working contact form script, please visit <a href="https://templatemo.com/contact" target="_parent">our contact page</a> for more info.
-                    
-                    <br><br>You are allowed to use this edu meeting CSS template for your school or university or business. You can feel free to modify or edit this layout. You are not allowed to redistribute the template ZIP file on any other template website. Please contact us for more information.
-                  </p>
-                  <div class="row">
-                    <div class="col-lg-4">
-                      <div class="hours">
-                        <h5>Hours</h5>
-                        <p>Monday - Friday: 07:00 AM - 13:00 PM<br>Saturday- Sunday: 09:00 AM - 15:00 PM</p>
-                      </div>
-                    </div>
-                    <div class="col-lg-4">
-                      <div class="location">
-                        <h5>Location</h5>
-                        <p>Recreio dos Bandeirantes, 
-                        <br>Rio de Janeiro - RJ, 22795-008, Brazil</p>
-                      </div>
-                    </div>
-                    <div class="col-lg-4">
-                      <div class="book now">
-                        <h5>Book Now</h5>
-                        <p>010-020-0340<br>090-080-0760</p>
-                      </div>
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="meeting-single-item">
+                            <div class="thumb">
+                                <div class="price">
+                                    <span>$14.00</span>
+                                </div>
+                                <div class="date">
+                                    <h6>Nov <span>12</span></h6>
+                                </div>
+                                <a href="meeting-details.html"><img src="assets/images/crop_img2.jpg" alt=""></a>
+                            </div>
+                            <div class="down-content">
+                                <a href="meeting-details.html"><h4>Introduction to Ranchi Agriculture College:</h4></a>
+                                <p style="font-size:16px;">Refers to the institution itself, which specializes in agricultural education and research.</p>
+                                <p style="text-align: justify; font-size:16px;">Ranchi Agriculture College (RAC) was established at Ranchi, as the second Agricultural College of Bihar, India in the year 1955. The first such institution was Bihar Agriculture College established in 1945 at Sabour in the district of Bhagalpur, Bihar. RAC was established to cater to the needs of the state for the different agricultural scenarios of the plateau region compared to the Bihar plains, such as hilly terrain, drought-prone areas with the absence of perennial rivers, small farm holdings, a large tribal population, and low farm productivity in major parts due to rainfed cultivation.</p><br>
+                                <p style="text-align: justify; font-size:16px;">Later on, two state Agricultural Universities were established: one at Pusa (Rajendra Agricultural University, Bihar) in 1971 and the other at Ranchi (Birsa Agricultural University, Jharkhand - <a href="http://www.bauranchi.org">www.bauranchi.org</a>) in 1981 to provide the necessary facilities to promote teaching, research, and extension activities in Agriculture, Veterinary Sciences, Animal Husbandry, and Forestry.</p><br>
+                                <p style="text-align: justify; font-size:16px;">RAC is part of the Birsa Agricultural University, BAU, Jharkhand, and is located in Kanke, Ranchi; which is 8 kilometers from downtown Ranchi and 17 kilometers from Ranchi Airport. RAC is a premier institution of the Jharkhand region offering both undergraduate and postgraduate programs in multiple disciplines, apart from conducting research and transferring technologies to farmers through extension activities.</p><br>
+                                
+                                <h4>The main objective behind the establishment of this College was:</h4><br>
+                                    To build a sound foundation for Agricultural sciences in the state & the country.
+                                    To create trained human resources for academic, research, and extension activities in various fields of Agriculture.
+                                   To promote science-based professional skills among the farming community for improving crop productivity and farm profits.
+                                     </p><br>
+                                <p style="text-align: justify; font-size:16px;">To achieve the above objectives, undergraduate and postgraduate students admitted each year are trained in different subjects of Agricultural Sciences, such as Soil Science, Plant Breeding & Genetics, Plant Pathology, Entomology, Agro Meteorology & Environmental Science, Agricultural Extension, Agricultural Economics, Agricultural Engineering, Horticulture, etc. The students, upon completion of their studies, join services under the state Department of Agriculture, Agricultural Universities, Banking sector, Fertilizer industry, Private firms, NGOs, etc.</p><br>
+                                
+                                <p style="text-align: justify; font-size:16px;">The first batch of students graduated from RAC in 1958. Since then, every year, students graduate and obtain undergraduate and postgraduate degrees. The Alumni of RAC, (subsequently of the Birsa Agricultural University), are spread all over the globe; some of them occupying/occupied responsible positions within the country and outside.</p><br>
+                                
+                                <h4 >The Birth of ARAC: A Dream Fulfilled by the Alumni:</h4><br>
+                                <p style="text-align: justify; font-size:16px;">On several occasions, the Alumni of RAC had met and realized the importance of having an Association of their own and for a formal Annual meet. The Alumni of the 1961 batch were more active in crystallizing the idea of having an Association. However, as per Dr. K.K. Sahay (1961 batch, article published in ARAC-2016 Souvenir), it is difficult to believe that the idea of having an annual meet/get-together of RAC graduates came around September 2002 at Niagara Falls, USA. There, he met his batchmate, Dr. Shyamalendu Sarkar, serving as Chairman of the Agricultural Economics Division of Saginaw University, Michigan, USA, and discussed the idea. Dr. Sahay discussed the matter with some of his batchmates and a couple of seniors located in Ranchi and Delhi.</p><br>
+                                
+                                <p style="text-align: justify; font-size:16px;">Subsequently, the idea crystallized into action in the first formal meeting of Alumni of Ranchi Agricultural College, which was held in the auditorium of RAC on December 28, 2003, under the presidency of the former Dean of RAC, Dr. A.K. Sarkar. During the meeting, Late Sri Priya Nath Verma (1960) and Sri Birendra Prasad (1961) were requested to draft the rules and regulations for ARAC and try for its registration during 2006 for its proper functioning. Late Sri Verma finalized the draft Constitution after thorough discussion with senior members of ARAC and critical scrutiny and incorporation of valid suggestions.</p><br>
+                                
+                                <p style="text-align: justify; font-size:16px;">This Constitution was adopted by the General Body meeting of the Alumni on November 20, 2008, wherein Dr. M.A. Mohsin, Dr. Kishore Kumar, Sri Ranoj Dasgupta, Sri Ashok Kumar, Late Dr. Ashok Kumar Mitra, Sri Birendra Prasad, and a few other Alumni (mostly from the 1961 batch) actively participated. Finally, the Association became a formal legal body with Registration No. 617 on November 11, 2009, under the Registrar General of Cooperatives, Government of Jharkhand. Since then, the RAC Alumni meet is being organized every year, with the exception of two years in between. The 9th Alumni meeting was organized on a grand scale by the sitting President, Dr. Abdul Wadood, on February 27-28, 2016. In 2018, Dr. Wadood successfully organized the 10th Alumni meet on February 24th and 25th, 2018.</p><br>
+                                
+                                <p style="text-align: justify; font-size:16px;">During this meeting on February 24th, Dr. Sarkar suggested adding two new tabs – “Counseling” and “Publications” to the Alumni website, which were added. The counseling tab aims to provide job search assistance to BAU graduates, and the publication page will be a reference page for important lectures and publications related to Agriculture. Alumni are encouraged to submit their papers to Admin for posting on the website.</p><br>
+                                
+                                <h4>Connecting Alumni Globally Through an ARAC Website:</h4><br>
+                                <p style="text-align: justify; font-size:16px;">An Alumnus of the 1966 batch, Dr. B K. Singh, developed a website for ARAC linked to his professional firm, Weather Sys, in 2015. However, despite the availability of several free websites on the Internet where any alumnus can register, there was no website for our alumni of its own, similar to those of other technical institutions. During the last General Body meeting, it was decided that the Alumni of Ranchi Agriculture College (ARAC) should have its own website.</p><br>
+                                <p style="text-align: justify; font-size:16px;"> The responsibility for designing and developing the website of ARAC, which would enable an Alumnus to freely register and search for their colleagues, seniors, and juniors based on their last name or year of graduation, was given to Shafiq Mallick (76), assisted by Shailendra K. Fauji (76), Mahesh C. Pathak (77), and Susmita Shrivastava (97). ARAC members are encouraged to give suggestions for improvement to this website.</p><br>
+
+                                <div class="share">
+                                    <h5>Share:</h5>
+                                    <ul>
+                                        <li><a href="#">Facebook</a></li>
+                                        <li><a href="#">Twitter</a></li>
+                                        <li><a href="#">LinkedIn</a></li>
+                                        <li><a href="#">Behance</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                     <div class="col-lg-12">
-                      <div class="share">
-                        <h5>Share:</h5>
-                        <ul>
-                          <li><a href="#">Facebook</a>,</li>
-                          <li><a href="#">Twitter</a>,</li>
-                          <li><a href="#">Linkedin</a>,</li>
-                          <li><a href="#">Behance</a></li>
-                        </ul>
-                      </div>
+                        <div class="main-button-red">
+                            <a href="meetings.html">Back To Meetings List</a>
+                        </div>
                     </div>
-                  </div>
                 </div>
-              </div>
             </div>
-            <div class="col-lg-12">
-              <div class="main-button-red">
-                <a href="meetings.html">Back To Meetings List</a>
-              </div>
-            </div>
-          </div>
         </div>
-      </div>
     </div>
-    <div class="footer">
-      <p>Copyright © 2022 Edu Meeting Co., Ltd. All Rights Reserved. 
-          <br>
-          Design: <a href="https://templatemo.com" target="_parent" title="free css templates">TemplateMo</a>
-          <br>
-          Distibuted By: <a href="https://themewagon.com" target="_blank" title="Build Better UI, Faster">ThemeWagon</a>
-        </p>
-    </div>
-  </section>
+    <!-- Include Footer -->
+   
+</section>
+
+<?php include 'footer.php'; ?>
 
 
   <!-- Scripts -->
--- footer.php
+++ footer.php
@@ -1,8 +1,30 @@
 <div class="footer">
-      <p>Copyright © 2022 Edu Meeting Co., Ltd. All Rights Reserved. 
-          <br>
-          Design: <a href="https://templatemo.com" target="_parent" title="free css templates">TemplateMo</a>
-          <br>
-          Distibuted By: <a href="https://themewagon.com" target="_blank" title="Build Better UI, Faster">ThemeWagon</a>
-        </p>
+    <div class="row">
+        <div class="col-lg-4">
+            <div class="hours">
+                <h5>Features and Events</h5>
+                <p>Monday - Friday: 07:00 AM - 01:00 PM<br>Saturday - Sunday: 09:00 AM - 03:00 PM</p>
+            </div>
+        </div>
+        <div class="col-lg-4">
+            <div class="location">
+                <h5>Location</h5>
+                <p>Kanke, Ranchi – 834006,<br>Jharkhand, India</p>
+            </div>
+        </div>
+        <div class="col-lg-4">
+            <div class="about-us">
+                <h5>About Us</h5>
+                <p>Ranchi Agriculture College is a premier institution of the<br>Jharkhand region offering both undergraduate and<br>postgraduate programs in multiple disciplines.</p>
+            </div>
+        </div>
     </div>
+
+    <div style="height: 30px;"></div> <!-- Adjust height as needed -->
+
+    <div class="footer-bottom">
+        <p>© 2016 Ranchi Agriculture College Alumni. All Rights Reserved | Website Developed by <a href="https://ssspatna.com/" target="_blank">Software Services and Solutions Patna</a> & <a href="http://dmllc.us/" target="_blank">DMLLC USA</a></p>
+        <p>© 2016 Vegetable Farm. All Rights Reserved | Design by <a href="https://w3layouts.com/" target="_blank">W3layouts</a></p>
+    </div>
+</div>
+
